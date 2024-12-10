f094dab8022cb98dc8e826eb3ba049f02ebf6ccb
София Бердникова <sofiytula71@gmail.com>
2024-12-10 19:24:15 +0300

    catalog.views выводит инфо о конкретном товаре по id

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -57,7 +57,11 @@ ROOT_URLCONF = "bookshop.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [
+            "templates",
+            "templates\\homepage",
+            "templates\\catalog",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- bookshop/catalog/views.py
+++ bookshop/catalog/views.py
@@ -1,4 +1,5 @@
 import django.http
+import django.shortcuts
 import django.views
 
 import catalog.models
@@ -8,10 +9,15 @@ class ItemList(django.views.View):
     template_name = "books.html"
 
     def get(self, request, *args, **kwaigs):
-        model = catalog.models.Book
-        return django.http.HttpResponse("Book")
+        return django.http.HttpResponse("Books")
+        # return django.shortcuts.render("book_list.html")
 
 
 class Item(django.views.View):
     def get(self, request, item_id, *args, **kwaigs):
-        return django.http.HttpResponse(f"{item_id}")
+        book = django.shortcuts.get_object_or_404(catalog.models.Book, id=item_id)
+        return django.shortcuts.render(
+            request,
+            "book_page.html",
+            context={"title": book.title, "description": book.description}
+        )
