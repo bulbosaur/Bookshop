10ed49cb1dc87b8235dbe34a33396343c93ffb37
Sofiya <sofiytula71@gmail.com>
2024-12-26 23:37:54 +0300

    working with font

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -74,12 +74,6 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 WSGI_APPLICATION = "bookshop.wsgi.application"
 
 
@@ -129,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
