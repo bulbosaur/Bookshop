571ac81a5999711fbfda3c568f3423b3e79c6e57
София Бердникова <sofiytula71@gmail.com>
2024-12-31 17:12:38 +0300

    add installation instructions

---
+++ README_ru.md
@@ -0,0 +1,44 @@
+# Bookshop
+##Установка и запуск
+
+1. Клонируйте репозиторий:
+
+```bash
+git clone https://github.com/username/bookshop.git
+cd bookshop
+```
+2. Создайте и активируйте виртуальное окружение:
+
+```bash
+python -m venv venv
+source venv/bin/activate  # для Linux/Mac
+venv\Scripts\activate  # для Windows
+```
+
+3. Установите зависимости:
+
+```bash
+pip install -r requirements/dev.txt   # установка зависимостей для разработки
+pip install -r requirements/prod.txt  # установка продакшн зависимостей
+```
+
+4. Создайте файл .env и настройте переменные окружения:
+
+5. Примените миграции:
+```bash
+python manage.py migrate
+```
+
+6. Создайте суперпользователя:
+
+```bash
+python manage.py createsuperuser
+```
+
+7. Запустите сервер:
+
+```bash
+python manage.py runserver
+```
+
+
--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -10,20 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f9!l==epftui2wv#e4k#%0b$v&(*k6ykgo1wvrc*q%3di=j&&)"
+SECRET_KEY = os.getenv("SECRET_KEY", "True") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.Getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
