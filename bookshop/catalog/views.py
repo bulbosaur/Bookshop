110de1e67c7aa21aba2e8ccad872fa9632bf939e
Sofiya <sofiytula71@gmail.com>
2025-02-04 22:44:03 +0300

    book catalog is displayed

--- bookshop/catalog/views.py
+++ bookshop/catalog/views.py
@@ -6,15 +6,18 @@ import catalog.models
 
 
 class ItemList(django.views.View):
-    template_name = "books.html"
 
-    def get(self, request, *args, **kwaigs):
-        return django.http.HttpResponse("Books")
-        # return django.shortcuts.render("book_list.html")
+    def get(self, request, *args, **kwargs):
+        catalog_book = catalog.models.Book.objects.all()
+        return django.shortcuts.render(
+            request,
+            "book_list.html",
+            context={"catalog_book": catalog_book},
+        )
 
 
 class Item(django.views.View):
-    def get(self, request, item_id, *args, **kwaigs):
+    def get(self, request, item_id, *args, **kwargs):
         book = django.shortcuts.get_object_or_404(catalog.models.Book, id=item_id)
         return django.shortcuts.render(
             request,
