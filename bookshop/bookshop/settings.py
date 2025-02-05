efe0d2a917b8cf7275d21a7fd387bd7785410710
Sofiya <sofiytula71@gmail.com>
2025-02-05 21:35:14 +0300

    fix debug

--- bookshop/bookshop/settings.py
+++ bookshop/bookshop/settings.py
@@ -25,10 +25,10 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "True") == "True"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "True")
+DEBUG = os.getenv("DEBUG", "False").lower() in ['true', '1', 't', 'y', 'yes']
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
